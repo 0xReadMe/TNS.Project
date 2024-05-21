0d9a8aaba9d5aeeb9f6793b68f287a9371e1507d
Marina Istomina <marinaistomina.04@mail.ru>
2024-05-21 22:51:26 +0300

    addUser.cs and datePicker

---
+++ TNS.Front-end/AddUser.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TNS.Front_end
+{
+    /// <summary>
+    /// Логика взаимодействия для AddUser.xaml
+    /// </summary>
+    public partial class AddUser : Window
+    {
+        public AddUser()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseDown_Minimized(object sender, MouseButtonEventArgs e)
+            => WindowState = WindowState.Minimized;
+        private void Image_MouseDown_Close(object sender, MouseButtonEventArgs e)
+            => Close();
+    }
+}
