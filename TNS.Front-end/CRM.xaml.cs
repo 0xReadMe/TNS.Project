7508e372197fa860631dc54dbf2b7ba7fe7729a5
ReadME <mezerasana@gmail.com>
2024-06-12 22:26:22 +0300

    refactor files in main

---
+++ TNS.Front-end/CRM.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TNS.Front_end
+{
+    /// <summary>
+    /// Логика взаимодействия для CRM.xaml
+    /// </summary>
+    public partial class CRM : Page
+    {
+        public CRM()
+        {
+            InitializeComponent();
+        }
+    }
+}
